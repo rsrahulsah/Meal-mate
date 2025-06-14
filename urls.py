ea5532067a3933aa020efcf87f11bd1ebd49673f
RAHUL KUMAR SAH <127338946+rsrahulsah@users.noreply.github.com>
2025-06-14 17:12:36 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,34 +1,23 @@
-from django.urls import path
-from . import views
- 
+"""
+URL configuration for meal_mate project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
-    # Home and authentication paths
-    path('', views.index, name='index'),
-    path('signin/', views.signin, name='signin'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.handle_login, name='handle_login'),
-    path('signup/submit/', views.handle_signup, name='handle_signup'),
-
-    # Restaurant-related paths
-    path('restaurants/add', views.add_restaurant_page, name='add_restaurant_page'),
-    path('restaurants/', views.show_restaurant_page, name='show_restaurant_page'),
-    path('restaurants/add/', views.add_restaurant, name='add_restaurant'),
-    path('restaurants/<int:restaurant_id>/menu/', views.restaurant_menu, name='restaurant_menu'),
-    path('restaurants/<int:restaurant_id>/update/', views.update_restaurant, name='update_restaurant'),
-    path('restaurants/<int:restaurant_id>/update/page/', views.update_restaurant_page, name='update_restaurant_page'),
-    path('restaurants/<int:restaurant_id>/delete/', views.delete_restaurant, name='delete_restaurant'),
-
-    path('menu/<int:menuItem_id>/update/', views.update_menuItem, name='update_menuItem'),
-    path('menu/<int:menuItem_id>/update/page/', views.update_menuItem_page, name='update_menuItem_page'),
-    path('menu/<int:menuItem_id>/delete/', views.delete_menuItem, name='delete_menuItem'),
-
-    path('restaurants/<int:restaurant_id>/menu/customer/<str:username>/', views.customer_menu, name='customer_menu'),
-    path('cart/<str:username>/', views.show_cart_page, name='show_cart_page'),
-    path('cart/<int:item_id>/add/<str:username>/', views.add_to_cart, name='add_to_cart'),
-
-    path('checkout/<str:username>/', views.checkout, name='checkout'),
-
-    path('orders/<str:username>/', views.orders, name='orders'),
-
-
+    path('admin/', admin.site.urls),
+    path('', include('delivery.urls')),
 ]
